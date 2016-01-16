a8a862acddef6b871e97e9fd070c0b105392972c
kyle smith <chessmonger2112@yahoo.com>
2016-01-15 19:37:29 -0800

    hard code

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_DB_CONN_CASTLE_BUILDING);
+// mongoose.connect(process.env.MONGO_DB_CONN_CASTLE_BUILDING);
+mongoose.connect("mongodb://kyle:pirate2006@ds037205.mongolab.com:37205/castle_building");
 
 app.use('/', routes);
 app.use('/users', users);
